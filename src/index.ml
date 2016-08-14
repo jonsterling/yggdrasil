67a6f1179bf3770ec8f0365487ffc93b50e59901
Darin Morrison <freebroccolo@users.noreply.github.com>
2016-08-14 17:48:03 -0600

    Make dimensions explicit; use Map instead of list

--- src/index.ml
+++ src/index.ml
@@ -1,3 +1,8 @@
+module Dm = struct
+  type t = int
+  [@@deriving eq, ord, show]
+end
+
 module Op = struct
   type t = string
   [@@deriving eq, ord, show]
@@ -71,62 +76,64 @@ end = struct
     { op; ar = { dom = []; cod }}
 end
 
-module Sign : sig
-  type t = Decl.t list
-  [@@deriving eq, ord, show]
-  val find : t -> Op.t -> Ar.t option
-end = struct
-  type t = Decl.t list
-  [@@deriving eq, ord, show]
-
-  let rec find gamma theta =
-    let open Decl in
-    match gamma with
-    | [] ->
-      None
-    | { op; ar } :: _ when Op.equal op theta ->
-      Some ar
-    | _ :: gamma ->
-      find gamma theta
-end
-
 module Computad : sig
-  type t = Sign.t list
-  [@@deriving eq, ord, show]
-  val find : t -> Op.t -> Ar.t option
+  type t
+  [@@deriving show]
+  val init : t
+  val bind : t -> Dm.t -> Decl.t -> t
+  val arity : t -> Op.t -> Ar.t option
+  val dimen : t -> Op.t -> Dm.t option
 end = struct
-  type t = Sign.t list
-  [@@deriving eq, ord, show]
+  open Decl
+  module Map = struct
+    include CCMap.Make(struct
+        type t = Op.t
+        let compare = Op.compare
+      end)
+  end
 
-  let rec find gamma op =
-    match gamma with
-    | [] ->
-      None
-    | sigma :: gamma ->
-      match Sign.find sigma op with
-      | None ->
-        find gamma op
-      | arity ->
-        arity
+  type t = {
+    ars : (Ar.t Map.t [@printer Map.print ~arrow:" := " Op.pp Ar.pp]);
+    dms : (Dm.t Map.t [@printer Map.print ~arrow:" := " Op.pp Dm.pp]);
+  }
+  [@@deriving show]
+
+  let init = {
+    ars = Map.empty;
+    dms = Map.empty;
+  }
+  let bind sg dm { op; ar } = {
+    dms = Map.add op dm sg.dms;
+    ars = Map.add op ar sg.ars;
+  }
+  let arity sg op =
+    try Some (Map.find op sg.ars) with Not_found -> None
+  let dimen sg op =
+    try Some (Map.find op sg.dms) with Not_found -> None
 end
 
 module Examples = struct
   open Ar
+  open Computad
   open Decl
   open Tm
 
+  let sg =
+    init
   let star =
     op "*"
 
-  let types : Sign.t =
-    ("bool" <! star) :: []
+  let sg =
+    bind sg 0 ("bool" <! star)
   let bool =
     op "bool"
 
-  let terms : Sign.t =
-    ("ff" <! bool) ::
-    ("tt" <! bool) ::
-    ("con" <: [ bool; bool ] --> bool) :: []
+  let sg =
+    bind sg 1 ("ff" <! bool)
+  let sg =
+    bind sg 1 ("tt" <! bool)
+  let sg =
+    bind sg 1 ("con" <: [ bool; bool ] --> bool)
   let ff =
     op "ff"
   let tt =
@@ -134,11 +141,14 @@ module Examples = struct
   let con =
     op "con"
 
-  let rules : Sign.t =
-    ("con-ff-ff" <: [ "con" *@ [ ff; ff ] ] --> ff) ::
-    ("con-ff-tt" <: [ "con" *@ [ ff; tt ] ] --> ff) ::
-    ("con-tt-ff" <: [ "con" *@ [ tt; ff ] ] --> ff) ::
-    ("con-tt-tt" <: [ "con" *@ [ tt; tt ] ] --> tt) :: []
+  let sg =
+    bind sg 2 ("con-ff-ff" <: [ "con" *@ [ ff; ff ] ] --> ff)
+  let sg =
+    bind sg 2 ("con-ff-tt" <: [ "con" *@ [ ff; tt ] ] --> ff)
+  let sg =
+    bind sg 2 ("con-tt-ff" <: [ "con" *@ [ tt; ff ] ] --> ff)
+  let sg =
+    bind sg 2 ("con-tt-tt" <: [ "con" *@ [ tt; tt ] ] --> tt)
   let con_ff_ff =
     op "con-ff-ff"
   let con_ff_tt =
@@ -148,9 +158,7 @@ module Examples = struct
   let con_tt_tt =
     op "con-tt-tt"
 
-  let computad : Computad.t =
-    [ types; terms; rules ]
-
   let () =
-    Printf.printf "%s\n" @@ Computad.show computad
+    Printf.printf "%s\n"
+    @@ Computad.show sg
 end
